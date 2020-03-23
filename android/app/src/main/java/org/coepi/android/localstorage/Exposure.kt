5a2df624126b1e9e6b69aa31b7ed515c3d0debe8
Sourabh Niyogi <sourabhniyogi@users.noreply.github.com>
2020-03-23 08:32:27 -0700

    Organize CoEpi Protocol Test (#3)

---
+++ android/app/src/main/java/org/coepi/android/localstorage/Exposure.kt
@@ -0,0 +1,8 @@
+package org.coepi.android.localstorage
+
+class Exposure(_symptoms: String, _contacts: List<Contact>?) {
+    var symptoms: String = _symptoms
+    var contacts: List<Contact>? = _contacts
+}
+
+
