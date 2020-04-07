ccd24d5c388a9cf6eff4b2073fb07d8fe33de57a
Brian Dickson <bdickson1@godaddy.com>
2020-04-07 16:53:40 -0700

    fix tyop

--- backend/backend.go
+++ backend/backend.go
@@ -127,7 +127,7 @@ func (backend *Backend) ProcessCENReport(cenReport *CENReport) (err error) {
 	for _, cenKey := range cenKeys {
 		cenKey := strings.Trim(cenKey, " \n")
 		if len(cenKey) > 62 && len(cenKey) <= 64 {
-			_, err = stmtKeys.Exec(cenKey, reportID, cenReport.ReportTimestamp)
+			_, err = stmtKeys.Exec(cenKey, reportID, cenReport.ReportTimeStamp)
 			if err != nil {
 				return err
 			}
